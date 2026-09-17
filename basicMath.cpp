7c563f7f1a17952e01eccd386604bd91c1914487
Lennon Cehajic <lennon.cehajic@stu.ocsb.ca>
2026-09-17 13:36:23 +0000

    Added basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2026 Lennon Cehajic All rights reserved.
+
+// .
+
+// Created by: Lennon Cehajic
+
+// Date: September 17th, 2026
+
+// This program runs basic math.
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "2+3 = " << 2+3 << std::endl;  // addition
+    std::cout << "7-5 = " << 7-5 << std::endl;  // subtraction
+    std::cout << "2x3 = " << 2*3 << std::endl;  // multiplication
+    std::cout << "10/2 = " << 10/2 << std::endl;  // division
+    std::cout << "15/6 = " << 15.0/6 << std::endl;  // decimal division
+    std::cout << "11 % 4 = " << 11 % 4 << std::endl;  // modulo
+    std::cout << "3^5 = " << pow(3, 5) << std::endl;  // exponents
+    std::cout << "√25 = " << sqrt(25) << std::endl;  // square root
+}
